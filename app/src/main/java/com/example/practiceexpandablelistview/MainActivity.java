fd46abdd5bc95b3e5c5a3e9f4a19cea6d0ffce3e
YukiMATSUOKA <yumitukai.yuki@gmail.com>
2021-05-14 02:36:08 +0900

    Practice ExpandableListView

---
+++ app/src/main/java/com/example/practiceexpandablelistview/MainActivity.java
@@ -0,0 +1,91 @@
+package com.example.practiceexpandablelistview;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ExpandableListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity
+        implements ExpandableListView.OnGroupClickListener,
+        ExpandableListView.OnChildClickListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        List<String> makers = getParentList();
+
+        List<List<String>> cars = getChileList();
+
+        ExpandableListView expandableListView = findViewById(R.id.expandable_list_view);
+        MyAdapter adapter = new MyAdapter(this, makers, cars);
+        for(int i = 0; i < adapter.getGroupCount(); i++){
+            if(adapter.getChildrenCount(i) == 0){
+
+            }
+        }
+        expandableListView.setAdapter(adapter);
+        expandableListView.setOnChildClickListener(this);
+        expandableListView.setOnGroupClickListener(this);
+
+    }
+
+    List<String> getParentList(){
+        List<String> makers = new ArrayList<>();
+        makers.add("TOYOTA");
+        makers.add("MAZDA");
+        makers.add("HONDA");
+        return makers;
+    }
+
+    List<List<String>> getChileList(){
+        List<String> cars_toyota = new ArrayList<>();
+        cars_toyota.add("CROWN");
+        cars_toyota.add("PRIUS");
+        cars_toyota.add("COROLLA");
+        cars_toyota.add("VITZ");
+        List<String> cars_mazda = new ArrayList<>();
+        cars_mazda.add("ATENZA");
+        cars_mazda.add("AXELA");
+        cars_mazda.add("DEMIO");
+        List<String> cars_honda = new ArrayList<>();
+//        cars_honda.add("LEGEND");
+//        cars_honda.add("CIVIC");
+//        cars_honda.add("FIT");
+        List<List<String>> cars = new ArrayList<>();
+        cars.add(cars_toyota);
+        cars.add(cars_mazda);
+        cars.add(cars_honda);
+        return cars;
+    }
+
+    @Override
+    public boolean onGroupClick(ExpandableListView expandableListView, View view, int i, long l) {
+        MyAdapter adapter = (MyAdapter) expandableListView.getExpandableListAdapter();
+        if(adapter.getChildrenCount(i) == 0){
+            String makerName =  (String)adapter.getGroup(i);
+            Toast.makeText(getApplicationContext(), makerName , Toast.LENGTH_LONG).show();
+        } else{
+            //do nothing
+        }
+        return false;
+    }
+
+    @Override
+    public boolean onChildClick(ExpandableListView expandableListView, View view, int i, int i1, long l) {
+        MyAdapter adapter = (MyAdapter) expandableListView.getExpandableListAdapter();
+        String makerName =  (String)adapter.getGroup(i);
+        String carName = (String)adapter.getChild(i, i1);
+        Toast.makeText(getApplicationContext(), makerName + " : " + carName, Toast.LENGTH_LONG).show();
+        return false;
+    }
+
+
+}
---
+++ app/src/main/java/com/example/practiceexpandablelistview/MyAdapter.java
@@ -0,0 +1,82 @@
+package com.example.practiceexpandablelistview;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseExpandableListAdapter;
+import android.widget.TextView;
+
+import java.util.List;
+
+public class MyAdapter extends BaseExpandableListAdapter {
+    List<String> mListMaker;
+    List<List<String>> mListCar;
+    Context mContext;
+
+    //コンストラクタ
+    MyAdapter (Context context, List<String> listMaker, List<List<String>> listCar) {
+        mContext = context;
+        mListMaker = listMaker;
+        mListCar = listCar;
+    }
+    @Override
+    public int getGroupCount() {
+        return mListMaker.size();
+    }
+
+    @Override
+    public int getChildrenCount(int i) {
+        return mListCar.get(i).size();
+    }
+
+    @Override
+    public Object getGroup(int i) {
+        return mListMaker.get(i);
+    }
+
+    @Override
+    public Object getChild(int i, int i1) {
+        return mListCar.get(i).get(i1);
+    }
+
+    @Override
+    public long getGroupId(int i) {
+        return 0;
+    }
+
+    @Override
+    public long getChildId(int i, int i1) {
+        return 0;
+    }
+
+    @Override
+    public boolean hasStableIds() {
+        return false;
+    }
+
+    @Override
+    public View getGroupView(int i, boolean b, View view, ViewGroup viewGroup) {
+        LayoutInflater inflater = (LayoutInflater) mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        view = inflater.inflate(R.layout.list_item_makers_layout, viewGroup, false);
+
+        TextView textView = view.findViewById(R.id.list_item_makers_name);
+        textView.setText(mListMaker.get(i));
+        return view;
+    }
+
+    @Override
+    public View getChildView(int i, int i1, boolean b, View view, ViewGroup viewGroup) {
+        LayoutInflater inflater = (LayoutInflater) mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        view = inflater.inflate(R.layout.list_item_cars_layout, viewGroup, false);
+
+        TextView textView = view.findViewById(R.id.list_item_cars_name);
+        textView.setText(mListCar.get(i).get(i1));
+        return view;
+    }
+
+    @Override
+    public boolean isChildSelectable(int i, int i1) {
+        return true;
+    }
+}
